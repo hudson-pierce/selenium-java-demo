c8efd8276773e47e4d937ea27bd130723b17400d
Hudson Pierce <hudsontpierce@gmail.com>
2023-02-14 16:14:42 -0500

    Add ProductsPage object and tests

---
+++ src/test/java/pages/ProductsPage.java
@@ -0,0 +1,85 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Sets up Page Objects and actions for the Products Page.
+ * @author Hudson Pierce
+ */
+public class ProductsPage {
+    @FindBy(css = "#header_container > div.header_secondary_container > span")
+    private WebElement productsHeader;
+
+    @FindBy(xpath = "//*[@id='header_container']/div[2]/div[2]/span/select")
+    private WebElement sortDropdown;
+
+    @FindBy(xpath = "//*[@id='item_4_title_link']/div")
+    private WebElement backpackName;
+
+    @FindBy(xpath = "//*[@id='item_0_title_link']/div")
+    private WebElement bikeLightName;
+
+    @FindBy(css = "#inventory_container > div > div:nth-child(1) > div.inventory_item_description > div.pricebar > div")
+    private WebElement backpackPrice;
+
+    @FindBy(css = "#inventory_container > div > div:nth-child(2) > div.inventory_item_description > div.pricebar > div")
+    private WebElement bikeLightPrice;
+
+    @FindBy(css = "#inventory_container > div > div:nth-child(1) > div.inventory_item_description > div.inventory_item_label > div")
+    private WebElement backpackDescription;
+
+    @FindBy(css = "#inventory_container > div > div:nth-child(2) > div.inventory_item_description > div.inventory_item_label > div")
+    private WebElement bikeLightDescription;
+
+    @FindBy(id = "add-to-cart-sauce-labs-backpack")
+    private WebElement backpackAddToCart;
+
+    @FindBy(id = "add-to-cart-sauce-labs-bike-light")
+    private WebElement bikeLightAddToCart;
+
+    @FindBy(css = "#shopping_cart_container > a > span")
+    private WebElement cartIcon;
+
+    /** Constructor method for setting up ProductsPage with driver and web elements. */
+    public ProductsPage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+    }
+
+    /** Add backpack to cart. */
+    public void addBackpackToCart() {
+        backpackAddToCart.click();
+    }
+
+    /** Add bike light to cart. */
+    public void addBikeLightToCart() {
+        bikeLightAddToCart.click();
+    }
+
+    //Assertions
+    /** Checks that backpack exists on the page. */
+    public void checkBackpackOnPage() {
+        assertEquals("Sauce Labs Backpack", backpackName.getText());
+        assertEquals("$29.99", backpackPrice.getText());
+        assertEquals("carry.allTheThings() with the sleek, streamlined Sly Pack that melds uncompromising style with unequaled laptop and tablet protection.", backpackDescription.getText());
+    }
+
+    /** Checks that bike light exists on the page. */
+    public void checkBikeLightOnPage() {
+        assertEquals("Sauce Labs Bike Light", bikeLightName.getText());
+        assertEquals("$9.99", bikeLightPrice.getText());
+        assertEquals("A red light isn't the desired state in testing but it sure helps when riding your bike at night. Water-resistant with 3 lighting modes, 1 AAA battery included.", bikeLightDescription.getText());
+    }
+
+    /** 
+    * Checks that the specified number of items are added to the cart. 
+    * @param items The number of items added to the cart.
+    */
+    public void checkItemsAddedToCart(int items) {
+        assertEquals(String.valueOf(items), cartIcon.getText());
+    }
+}
---
+++ src/test/java/tests/ProductsTest.java
@@ -0,0 +1,57 @@
+package tests;
+
+import pages.LoginPage;
+import pages.ProductsPage;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.WebDriver;
+
+import constants.Constants;
+import utils.Drivers;
+
+/**
+ * Test suite for the Products page
+ * @author Hudson Pierce
+ */
+public class ProductsTest {
+    WebDriver driver;
+    private ProductsPage productsPage;
+
+    /** Sets up driver and products page before each test. */
+    @BeforeEach
+    public void setup() {
+        driver = (new Drivers()).setup();
+        productsPage = new ProductsPage(driver);
+
+        // login to access products page
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.login(Constants.VALID_USERNAME, Constants.VALID_PASSWORD);
+    }
+
+    /** Closes the driver after each test. */
+    @AfterEach
+    public void teardown() {
+        driver.close();
+    }
+
+    /** Verifies that an item is added to the cart. */
+    @Test
+    public void addItemToCart() {
+        productsPage.checkBackpackOnPage();
+        productsPage.addBackpackToCart();
+        productsPage.checkItemsAddedToCart(1);
+    }
+
+    /** Verifies that multiple items are added to the cart. */
+    @Test
+    public void addMultipleItemsToCart() {
+        // add backpack
+        productsPage.checkBackpackOnPage();
+        productsPage.addBackpackToCart();
+        productsPage.checkItemsAddedToCart(1);
+
+        // add bike light
+        productsPage.checkBikeLightOnPage();
+        productsPage.addBikeLightToCart();
+        productsPage.checkItemsAddedToCart(2);
+    }
+}
